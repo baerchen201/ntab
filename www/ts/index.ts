50aa129ac6b8f6e8ea691d975d774a4173042b99
baer1 <videocreator@outlook.de>
2024-10-02 23:45:11 +0200

    Add docstrings to typescript
    This took longer than I thought it would, documenting code is hard

--- www/ts/index.ts
+++ www/ts/index.ts
@@ -22,6 +22,11 @@ const WidgetTypes: { [key: string]: new () => Widget } = {
   },
 };
 
+/**
+ * Read widget list from memory as JSON Object
+ * If the memory is corrupted, ask user if they want to reset it.
+ * @returns The widgets from memory, null if memory is corrupted and the user declines the reset
+ */
 function getWidgets(): Widget[] | null {
   let json: string | null = localStorage.getItem("widgets");
   try {
@@ -35,24 +40,46 @@ function getWidgets(): Widget[] | null {
   }
 }
 
+/**
+ * Overwrite the currently saved list of widgets
+ * @param widgets List of widgets to save
+ * @returns `<widgets>` parameter, useful for chaining calls `console.log(_setWidgets([]))`
+ */
 function _setWidgets(widgets: Widget[] = []): Widget[] {
   localStorage.setItem("widgets", JSON.stringify(widgets));
   return widgets;
 }
 
+/**
+ * Remove widget at id `<id>` from memory
+ * @param id The widget id to remove
+ * @returns The updated list of widgets
+ */
 function removeWidget(id: number): Widget[] {
   let widgets: Widget[] = getWidgets()!;
   widgets.splice(id, 1);
   return _setWidgets(widgets);
 }
 
-function addWidget(widget: Widget, insert?: number) {
+/**
+ * Save widget to memory at position `<insert>`
+ * @param widget The widget object to save
+ * @param insert The position at which to insert the widget, default -1 (last)
+ * @returns The updated list of widgets
+ */
+function addWidget(widget: Widget, insert?: number): Widget[] {
   let widgets: Widget[] = getWidgets()!;
   if (insert == undefined) widgets.push(widget);
   else widgets.splice(insert, 0, widget);
   return _setWidgets(widgets);
 }
 
+/**
+ * Create a Widget Object for use with `addWidget`
+ * @param type The type of widget to create (see `WidgetTypes`)
+ * @param options The init options of the widget
+ * @returns The created widget
+ */
 function createWidgetObject(
   type: string,
   options?: { [key: string]: string | boolean | number | null }
