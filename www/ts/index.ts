f3988dd4c4576be1b494b9945d99db699a6f5812
baer1 <videocreator@outlook.de>
2024-10-03 16:17:26 +0200

    Update widget memory and add widget to HTML

--- www/ts/index.ts
+++ www/ts/index.ts
@@ -4,48 +4,58 @@ window.addEventListener("load", () => {
   });
 });
 
-interface Widget {
+interface JSONWidget {
   type: number;
-  options: { [key: string]: string | boolean | number | null };
+  options: { [key: string]: string };
 }
 
-const WidgetTypes: { [key: string]: new () => Widget } = {
-  TimeWidget: class implements Widget {
-    type = 0;
-    options: { "12h": boolean } = { "12h": false };
-  },
-  DateWidget: class implements Widget {
-    type = 1;
-    options: { format: "normal" | "switched" | "reverse" } = {
-      format: "normal",
-    };
-  },
-};
+class Widget extends HTMLElement {
+  type: number;
+  options: { [key: string]: string };
+
+  constructor(type: number, options: { [key: string]: string } = {}) {
+    super();
+
+    this.type = type;
+    this.options = options;
+  }
+
+  static fromJSON(json: JSONWidget): Widget {
+    return new Widget(json.type, json.options);
+  }
+
+  toJSON(): JSONWidget {
+    return { type: this.type, options: this.options };
+  }
+}
+
+window.customElements.define("ntab-widget", Widget);
 
 /**
- * Read widget list from memory as JSON Object
+ * Read widget list from memory as `JSONWidget[]`
  * If the memory is corrupted, ask user if they want to reset it.
  * @returns The widgets from memory, null if memory is corrupted and the user declines the reset
  */
-function getWidgets(): Widget[] | null {
+function getStoredWidgets(): JSONWidget[] | null {
   let json: string | null = localStorage.getItem("widgets");
   try {
     return JSON.parse(json == null ? "[]" : json);
   } catch (e) {
     return window.confirm(
-      "An error occurred while reading the saved widgets list.\nWould you like to reset it?"
+      "An error occurred while reading the saved widgets list.\nWould you like to reset it?\n" +
+        String(e)
     )
-      ? _setWidgets()
+      ? _overwriteStoredWidgets([])
       : null;
   }
 }
 
 /**
- * Overwrite the currently saved list of widgets
+ * Overwrite the currently stored list of widgets
  * @param widgets List of widgets to save
- * @returns `<widgets>` parameter, useful for chaining calls `console.log(_setWidgets([]))`
+ * @returns `<widgets>` parameter
  */
-function _setWidgets(widgets: Widget[] = []): Widget[] {
+function _overwriteStoredWidgets(widgets: JSONWidget[]): JSONWidget[] {
   localStorage.setItem("widgets", JSON.stringify(widgets));
   return widgets;
 }
@@ -55,46 +65,53 @@ function _setWidgets(widgets: Widget[] = []): Widget[] {
  * @param id The widget id to remove
  * @returns The updated list of widgets
  */
-function removeWidget(id: number): Widget[] {
-  let widgets: Widget[] = getWidgets()!;
+function removeStoredWidget(id: number): JSONWidget[] {
+  let widgets: JSONWidget[] = getStoredWidgets()!;
   widgets.splice(id, 1);
-  return _setWidgets(widgets);
+  return _overwriteStoredWidgets(widgets);
 }
 
 /**
- * Save widget to memory at position `<insert>`
+ * Save widget to memory at position `<offset>`
  * @param widget The widget object to save
- * @param insert The position at which to insert the widget, default -1 (last)
+ * @param offset The position at which to insert the widget, default -1 (last)
  * @returns The updated list of widgets
  */
-function addWidget(widget: Widget, insert?: number): Widget[] {
-  let widgets: Widget[] = getWidgets()!;
-  if (insert == undefined) widgets.push(widget);
-  else widgets.splice(insert, 0, widget);
-  return _setWidgets(widgets);
+function insertWidget(widget: JSONWidget, offset?: number): JSONWidget[] {
+  let widgets: JSONWidget[] = getStoredWidgets()!;
+  if (offset == undefined) widgets.push(widget);
+  else widgets.splice(offset, 0, widget);
+  return _overwriteStoredWidgets(widgets);
 }
 
 /**
+ * @deprecated Use `new Widget(<type>, <options>).toJSON()` instead
  * Create a Widget Object for use with `addWidget`
  * @param type The type of widget to create (see `WidgetTypes`)
  * @param options The init options of the widget
  * @returns The created widget
  */
-function createWidgetObject(
-  type: string,
-  options?: { [key: string]: string | boolean | number | null }
-): Widget {
-  let widget: Widget = new WidgetTypes[type]();
-  if (options)
-    Object.keys(widget.options).forEach((key) => {
-      if (Object.keys(options).includes(key))
-        widget.options[key] = options[key];
-    });
-  return widget;
+function createJSONWidget(
+  type: number,
+  options: { [key: string]: string } = {}
+): JSONWidget {
+  return { type: type, options: options };
 }
 
-_setWidgets(); // Clear stored widget memory
-addWidget(createWidgetObject("DateWidget")); // Create DateWidget object
-addWidget(createWidgetObject("TimeWidget")); // Create TimeWidget object
-addWidget(createWidgetObject("TimeWidget", { "12h": true })); // Create TimeWidget object with options
-console.log(getWidgets()); // Print saved objects
+_overwriteStoredWidgets([]); // Clear stored widget memory
+//! Deprecated
+insertWidget(createJSONWidget(2)); // Create DateWidget object
+insertWidget(createJSONWidget(1)); // Create TimeWidget object
+insertWidget(createJSONWidget(1, { "12h": "true" })); // Create TimeWidget object with options
+console.log(getStoredWidgets()); // Print saved objects
+
+_overwriteStoredWidgets([]); // Clear stored widget memory
+insertWidget(new Widget(2).toJSON()); // Create DateWidget object
+insertWidget(new Widget(1).toJSON()); // Create TimeWidget object
+insertWidget(new Widget(1, { "12h": "true" }).toJSON()); // Create TimeWidget object with options
+console.log(getStoredWidgets()); // Print saved objects
+
+getStoredWidgets()!.forEach((json: JSONWidget) => {
+  let widget = Widget.fromJSON(json);
+  document.body.appendChild(widget);
+});
